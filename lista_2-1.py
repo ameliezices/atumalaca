9b921891de15401bdede5a83ae0ca96c0934498d
ameliezices <carvalho.lima@ufms.br>
2025-10-23 09:53:20 -0400

    Add input handling for two integer sequences
    Implement input and validation for two integer sequences.

---
+++ lista_2-1.py
@@ -0,0 +1,9 @@
+seq1 = input("Digite uma sequência de números inteiros, separando-os por espaços: ")
+lst1 = [int(i1) for i1 in seq1.split()]
+seq2 = input("Digite outra sequência de números inteiros, também separando-os por espaços: ")
+lst2 = [int(i2) for i2 in seq2.split()]
+if len(lst1) > len(lst2):
+  print("A primeira sequência não pode ser maior que a primeira. Tente novamente inserindo primeiro uma sequência menor ou igual à outra.")
+else:
+  contador = 0
+  for i in range()
