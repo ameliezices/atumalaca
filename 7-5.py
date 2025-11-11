6de61abb6404b4db76f9d9929c333232c46c29f1
ameliezices <carvalho.lima@ufms.br>
2025-11-11 10:34:22 -0400

    Add files via upload

---
+++ 7-5.py
@@ -0,0 +1,5 @@
+'''
+Dada uma lista de n elementos, contendo os resultados dos lançamentos de um dado, determinar o número de ocorrências de cada face.
+(Descobrir se um dado está "viciado" ou não!)
+OBS: Listas 7 e 11!
+'''
---
+++ raiz.py
@@ -0,0 +1,3 @@
+n = int(input("Forneça um número natural: "))
+raiz = n ** (n / 0.5)
+print(f"A raiz quadrada de {n} é {raiz}")
